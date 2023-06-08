31fad6895c88076f6621aeaff27886ef8738db33
PABi <106952246+PABixn@users.noreply.github.com>
2023-06-09 01:02:14 +0200

    Add more social functions

--- Chessin/src/main/java/com/chessin/controller/api/UserController.java
+++ Chessin/src/main/java/com/chessin/controller/api/UserController.java
@@ -2,12 +2,14 @@ package com.chessin.controller.api;
 
 import com.chessin.controller.register.UserService;
 import com.chessin.controller.requests.FriendInvitationRequest;
+import com.chessin.controller.requests.FriendInvitationResponseRequest;
 import com.chessin.controller.responses.FriendInvitationResponse;
 import com.chessin.model.register.user.User;
 import com.chessin.controller.responses.UserResponse;
 import com.chessin.model.register.user.UserRepository;
 import com.chessin.model.social.FriendInvitation;
 import com.chessin.model.social.FriendInvitationRepository;
+import com.chessin.model.social.FriendInvitationResponseType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -62,5 +64,75 @@ public class UserController {
             responses.add(FriendInvitationResponse.fromFriendInvitation(invitation));
 
         return ResponseEntity.ok().body(responses);
+    }
+
+    @PostMapping("/respondToInvitation")
+    public ResponseEntity<?> respondToInvitation(@RequestBody FriendInvitationResponseRequest request)
+    {
+        if(!friendInvitationRepository.existsByUserEmailAndFriendEmail(request.getEmail(), request.getFriendEmail()))
+            return ResponseEntity.badRequest().body("Invitation does not exist.");
+
+        friendInvitationRepository.deleteByUserEmailAndFriendEmail(request.getEmail(), request.getFriendEmail());
+
+        if(request.getResponseType() == FriendInvitationResponseType.ACCEPT)
+        {
+            User user = userRepository.findByEmail(request.getEmail()).get();
+            User friend = userRepository.findByEmail(request.getFriendEmail()).get();
+
+            user.getFriends().add(friend);
+            friend.getFriends().add(user);
+
+            userRepository.save(user);
+            userRepository.save(friend);
+        }
+
+        return ResponseEntity.ok().body("Invitation responded");
+    }
+
+    @PostMapping("/getFriends/{email}")
+    public ResponseEntity<?> getFriends(@PathVariable String email)
+    {
+        if(!userRepository.existsByEmail(email))
+            return ResponseEntity.badRequest().body("User does not exist");
+
+        User user = userRepository.findByEmail(email).get();
+
+        List<UserResponse> friends = new ArrayList<>();
+
+        for(User friend : user.getFriends())
+            friends.add(UserResponse.fromUser(friend));
+
+        return ResponseEntity.ok().body(friends);
+    }
+
+    @PostMapping("/removeFriend")
+    public ResponseEntity<?> removeFriend(@RequestBody FriendInvitationRequest request)
+    {
+        if(!userRepository.existsByEmail(request.getEmail()))
+            return ResponseEntity.badRequest().body("User does not exist.");
+        else if(!userRepository.existsByEmail(request.getFriendEmail()))
+            return ResponseEntity.badRequest().body("Friend does not exist.");
+
+        User user = userRepository.findByEmail(request.getEmail()).get();
+        User friend = userRepository.findByEmail(request.getFriendEmail()).get();
+
+        user.getFriends().remove(friend);
+        friend.getFriends().remove(user);
+
+        userRepository.save(user);
+        userRepository.save(friend);
+
+        return ResponseEntity.ok().body("Friend removed.");
+    }
+
+    @PostMapping("/removeInvitation")
+    public ResponseEntity<?> removeInvitation(@RequestBody FriendInvitationRequest request)
+    {
+        if(!friendInvitationRepository.existsByUserEmailAndFriendEmail(request.getEmail(), request.getFriendEmail()))
+            return ResponseEntity.badRequest().body("Invitation does not exist.");
+
+        friendInvitationRepository.deleteByUserEmailAndFriendEmail(request.getEmail(), request.getFriendEmail());
+
+        return ResponseEntity.ok().body("Invitation removed.");
     }
 }
